b12877a98d36490e6ecc1643c5fdc5d37a391901
margotchab <153506591+margotchab@users.noreply.github.com>
2024-04-15 17:50:30 +0200

    correction de fautes

--- src/main/java/fr/uga/miashs/dciss/chatservice/server/GroupMsg.java
+++ src/main/java/fr/uga/miashs/dciss/chatservice/server/GroupMsg.java
@@ -37,7 +37,7 @@ public class GroupMsg implements PacketProcessor {
 	
 	/**
 	 * This method has to be used to add a member to the group.
-	 * It update the bidirectional relationship, i.e. the user is added to the group and the the group is added to the user.
+	 * It updates the bidirectional relationship, i.e. the user is added to the group and the group is added to the user.
 	 * @param s
 	 * @return
 	 */
@@ -47,7 +47,7 @@ public class GroupMsg implements PacketProcessor {
 	
 	/**
 	 * This method has to be used to remove a member from the group.
-	 * It update the bidirectional relationship, i.e. the user is removed from the group and the the group is removed from the user.
+	 * It updates the bidirectional relationship, i.e. the user is removed from the group and the group is removed from the user.
 	 * @param s
 	 * @return
 	 */
@@ -66,7 +66,7 @@ public class GroupMsg implements PacketProcessor {
 		members.stream().filter(m->m.getId()!=p.srcId).forEach( m -> m.process(p));
 	}
 	
-	// to be used carrefully, because it does not update birectional relationship in case of addition or removal.
+	// to be used carefully, because it does not update birectional relationship in case of addition or removal.
 	protected Set<UserMsg> getMembers() {
 		return members;
 	}
